d82adec0943393d43500ab7db83e2960a6e49fb9
DSerio <davide@serio.cloud>
2025-04-08 07:44:14 +0200

    Deploy website - based on b8569e4e79fd4345621a8071428d3480b300e711

---
+++ assets/js/5e95c892.a5dd8efd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvelero_docs=self.webpackChunkvelero_docs||[]).push([[9647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(6540);var c=r(4164),u=r(1769),a=r(204),l=r(2831),o=r(1410),d=r(4848);function n(e){return(0,d.jsx)(u.e3,{className:(0,c.A)(a.G.wrapper.docsPages),children:(0,d.jsx)(o.A,{children:(0,l.v)(e.route.routes)})})}}}]);
--- assets/js/acecf23e.ec26671e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkvelero_docs=self.webpackChunkvelero_docs||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
